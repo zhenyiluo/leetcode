33c417223f32e74ef2d4676d09dd86b9b3795184
Zhenyi Luo <zhenyiluo@gmail.com>
2015-08-30 16:43:34 -0400

    Basic Calculator

--- src/BasicCalculator/Solution.java
+++ src/BasicCalculator/Solution.java
@@ -2,54 +2,40 @@ import java.util.Stack;
 
 public class Solution {
     public static void main(String[] args){
-        System.out.println(new Solution().calculate(" 2-1 + 2 "));
+        System.out.println(new Solution().calculate("1 - (5)"));
     }
     public int calculate(String s) {
-        Stack<Integer> st1 = new Stack<Integer>();
-        Stack<Character> st2 = new Stack<Character>();
-        for(int i = 0; i < s.length(); ){
+        Stack<Integer> stack = new Stack<Integer>();
+        int result = 0;
+        int sign = 1;
+        int num = 0;
+        for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            switch(c){
-                case ' ' : i++ ; break;
-                case '+' :
-                case '-' : 
-                case '(' : i ++ ; st2.push(c); break;
-                case ')' : if(st2.peek() == '('){
-                            st2.pop();
-                            if(!st2.isEmpty() && st2.peek() != '('){
-                                int val2 = st1.pop();
-                                int val1 = st1.pop();
-                                if(st2.peek() == '+'){
-                                    st1.push(val1 + val2);
-                                }else{
-                                    st1.push(val1 - val2);
-                                }
-                                st2.pop();
-                            }
-                }else{
-                    System.out.println("Something is wrong");
-                }
-                i ++;
-                break;
-                default : int num = c - '0'; i++;
-                while(i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9'){
-                    num = num * 10 + s.charAt(i) - '0';
-                    i++;
-                }
-                if(!st2.isEmpty() && st2.peek() != '('){
-                    int val = st1.pop();
-                    char cc = st2.pop();
-                    if(cc == '+'){
-                        st1.push(val + num);
-                    }else{
-                        st1.push(val - num);
-                    }
-                }else{
-                    st1.push(num);
-                }
-                break;
+            if(c >= '0' && c <= '9'){
+                num = num * 10 + c- '0';
+            }else if(c == '+'){
+                result += sign * num;
+                sign = 1;
+                num = 0;
+            }else if(c == '-'){
+                result += sign * num;
+                sign = -1;
+                num = 0;
+            }else if(c == '('){
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            }else if(c == ')'){
+                result += sign * num;
+                num = 0;
+                result *= stack.pop();
+                result += stack.pop();
             }
         }
-        return st1.peek();
+        if(num != 0){
+            result += sign * num;
+        }
+        return result;
     }
 }
