507c1b63077e29dbcf948a35fa24c44337e9b9aa
Zhenyi Luo <zhenyiluo@gmail.com>
2015-08-30 22:44:15 -0400

    Power of Two

---
+++ src/PowerOfTwo/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n < 1){
+            return false;
+        }
+        while(n != 1){
+            if(n % 2 != 0){
+                return false;
+            }else{
+                n /= 2;
+            }
+        }
+        return true;
+    }
+}
