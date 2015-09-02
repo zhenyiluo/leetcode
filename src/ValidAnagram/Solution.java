cdf85bc92b2bdbf35f52cc15714be9a20b7a56dc
Zhenyi Luo <eric@Zhenyis-MacBook-Pro.local>
2015-09-02 18:41:28 -0400

    Valid Anagram

---
+++ src/ValidAnagram/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null || s.length() != t.length()){
+            return false;
+        }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+}
