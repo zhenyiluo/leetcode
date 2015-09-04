f0bd9fd64a911dddfea2cf7253bcfa065f25edf1
Zhenyi Luo <zhenyiluo@gmail.com>
2015-09-03 20:43:04 -0400

    H-Index

---
+++ src/HIndex/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length <1){
+            return 0;
+        }
+        Arrays.sort(citations);
+        for(int i = 0; i < citations.length; i++){
+            if(citations[i] >= citations.length - i){
+                return citations.length - i;
+            }
+        }
+        return 0;
+    }
+}
