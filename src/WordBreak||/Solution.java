69e597939b8b667ab00c31b00888a82ab3ec1901
zhenyiluo <zhenyiluo@gmail.com>
2018-02-27 09:10:59 -0500

    word breakk I & II

--- src/WordBreak||/Solution.java
+++ src/WordBreak||/Solution.java
@@ -1,27 +1,24 @@
-public class Solution {
-    HashMap<String, List<String>> hm = new HashMap<String, List<String>>();
-    public List<String> wordBreak(String s, Set<String> wordDict) {
-        if(!hm.containsKey(s)){
-            if(s == null || s.length() == 0){
-                List<String> list = new LinkedList<String>();
-                list.add("");
-                hm.put(s, list);
-                return hm.get(s);
-            }
-            List<String> list = new LinkedList<String>();
-            for(String prefix : wordDict){
-                if(s.length() >= prefix.length() && s.substring(0, prefix.length()).equals(prefix)){
-                    for(String tmpS : wordBreak(s.substring(prefix.length()), wordDict)){
-                        if(tmpS.length() == 0){
-                            list.add(prefix);
-                        }else{
-                            list.add(prefix + " " + tmpS);
-                        }
+class Solution {
+    HashMap<String, List<String>> cache = new HashMap<>();
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        if(cache.containsKey(s)){
+            return cache.get(s);
+        }
+        List<String> res = new ArrayList<>();
+        for(String prefix: wordDict){
+            if(s.startsWith(prefix)){
+                if(prefix.length() == s.length()){
+                    res.add(prefix);
+                    continue;
+                }else{
+                    List<String> strList = wordBreak(s.substring(prefix.length()), wordDict);
+                    for(String str: strList){
+                        res.add(prefix + " " + str);
                     }
                 }
             }
-            hm.put(s, list);
         }
-        return hm.get(s);
+        cache.put(s, res);
+        return res;
     }
 }
