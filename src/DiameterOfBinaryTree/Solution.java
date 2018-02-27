575ff8b28dcb6ff77f5aed0698a6ff7ab92e1747
zhenyiluo <zhenyiluo@gmail.com>
2018-02-27 10:00:19 -0500

    Diameter of binary tree

---
+++ src/DiameterOfBinaryTree/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root){
+        if(root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        max = Math.max(max, left + right);
+        return 1 + Math.max(left, right);
+    }
+}
