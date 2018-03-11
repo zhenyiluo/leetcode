cf63f234a6be48b88f05f0485ed1d602d78674ac
zhenyiluo <zhenyiluo@gmail.com>
2018-03-11 11:09:58 -0400

    Combination Sum 3

--- src/CombinationSum|||/Solution.java
+++ src/CombinationSum|||/Solution.java
@@ -1,34 +1,27 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ret = new LinkedList<List<Integer>>();
-        if(k <= 0 || k >9){
-            return ret;
-        }
-        if(n < (1+k)*k/2){
-            return ret;
-        }
-        if(n > (19-k) * k /2 ){
-            return ret;
-        }
-        return dfs(k, n, 0);
+        List<List<Integer>> ret = new ArrayList<>();
+        if(k <= 0 || n <= 0) return ret;
+        dfs(ret, new ArrayList<>(), k, n, 1);
+        return ret;
     }
-    
-    private List<List<Integer>> dfs(int k, int n, int num){
-        List<List<Integer>> ret = new LinkedList<List<Integer>>();
-        if(k < 0 || n < 0){
-            return ret;
-        }
-        if(k == 0 && n == 0){
-            ret.add(new LinkedList<Integer>());
-            return ret;
-        }
-        for(int i = num +1; i <=9; i++){
-            for(List<Integer> list : dfs(k-1, n-i, i)){
-                list.add(0, i);
-                ret.add(list);
+
+    private void dfs(List<List<Integer>> ret, List<Integer> tmpL, int k, int n, int start){
+        if(k== 0){
+            if(n == 0){
+                ret.add(new ArrayList<>(tmpL));
+                return;
+            }else{
+                return;
             }
         }
-        
-        return ret;
+        if(n < 0){
+            return;
+        }
+        for(int i = start; i <= 9; i++){
+            tmpL.add(i);
+            dfs(ret, tmpL, k-1, n-i, i+1);
+            tmpL.remove(tmpL.size() -1);
+        }
     }
 }
