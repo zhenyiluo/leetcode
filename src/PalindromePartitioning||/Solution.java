b9f043136b73a20f326e1b216a3636814587c03b
zhenyiluo <zhenyiluo@gmail.com>
2018-03-11 13:35:04 -0400

    Palindrome Partition II

--- src/PalindromePartitioning||/Solution.java
+++ src/PalindromePartitioning||/Solution.java
@@ -1,23 +1,20 @@
-public class Solution {
+class Solution {
     public int minCut(String s) {
-        if(s == null || s.length() == 0){
-            return 0;
-        }
-        int[] minCut = new int[s.length()+1];
+        if(s == null || s.length() == 0) return 0;
+        int[] minCut = new int[s.length() + 1];
         for(int i = 0; i < s.length(); i++){
-            minCut[i] = s.length() - i;
+            minCut[i] = s.length();
         }
-        
+
         boolean[][] dp = new boolean[s.length()][s.length()];
-        for(int i = s.length() -1; i>= 0; i--){
+        for(int i = s.length() -1 ; i >= 0; i--){
             for(int j = i; j < s.length(); j++){
-                if( (s.charAt(i) == s.charAt(j)) && ((j - i <2) ||(dp[i+1][j-1]) )){
+                if(s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i+1][j-1])){
                     dp[i][j] = true;
                     minCut[i] = Math.min(minCut[i], 1 + minCut[j+1]);
                 }
             }
         }
-        
         return minCut[0] -1;
     }
 }
