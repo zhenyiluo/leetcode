48a7954ea1b0f41796a4d0bc7ae7b6f0c1fb3be8
zhenyiluo <zhenyiluo@gmail.com>
2018-03-11 23:54:32 -0400

    Search a 2D matrix

--- src/SearchA2DMatrix||/Solution.java
+++ src/SearchA2DMatrix||/Solution.java
@@ -1,19 +1,18 @@
-public class Solution {
+class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
             return false;
         }
-        int m = matrix.length; 
-        int n = matrix[0].length;
-        int x = 0; 
-        int y = n-1;
-        while(x < m && y >=0){
-            if(target == matrix[x][y]){
-                return true;
-            }else if(target > matrix[x][y]){
-                x++;
+
+        int row = 0;
+        int col = matrix[0].length -1;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] < target){
+                row ++;
+            }else if(matrix[row][col] > target){
+                col --;
             }else{
-                y--;
+                return true;
             }
         }
         return false;
