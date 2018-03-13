c2d33887df03511b67b1a19c1a6cb22dcc4f523d
zhenyiluo <zhenyiluo@gmail.com>
2018-03-12 23:51:44 -0400

    Find K Closest Elements

---
+++ src/FindKClosestElements/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int start = 0;
+        int end = arr.length - k;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(x - arr[mid] > arr[mid + k] - x){
+                start = mid + 1;
+            }else{
+                end = mid;
+            }
+        }
+        List<Integer> ret = new LinkedList<>();
+        for(int i = start; i < start + k; i++){
+            ret.add(arr[i]);
+        }
+        return ret;
+    }
+}
