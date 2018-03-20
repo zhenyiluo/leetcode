7303881faa8cec3be2b660f6211c90815051753d
zhenyiluo <zhenyiluo@gmail.com>
2018-03-20 10:09:05 -0400

    Binary Tree Level order traversal

--- src/BinaryTreeLevelOrderTraversal/Solution.java
+++ src/BinaryTreeLevelOrderTraversal/Solution.java
@@ -12,30 +12,17 @@ class Solution {
         List<List<Integer>> ret = new ArrayList<>();
         if(root == null) return ret;
         Queue<TreeNode> qNode = new LinkedList<>();
-        Queue<Integer> qLevel = new LinkedList<>();
         qNode.add(root);
-        qLevel.add(0);
-        int curLevel = -1;
         while(!qNode.isEmpty()){
-            int level = qLevel.poll();
-            TreeNode node = qNode.poll();
-            if(level != curLevel){
-                List<Integer> list = new ArrayList<>();
-                list.add(node.val);
-                ret.add(list);
-                curLevel = level;
-            }else{
-                List<Integer> list = ret.get(ret.size() -1);
+            int size = qNode.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = qNode.poll();
+                if(node.left != null) qNode.add(node.left);
+                if(node.right != null) qNode.add(node.right);
                 list.add(node.val);
             }
-            if(node.left != null){
-                qNode.add(node.left);
-                qLevel.add(curLevel+1);
-            }
-            if(node.right != null){
-                qNode.add(node.right);
-                qLevel.add(curLevel +1);
-            }
+            ret.add(list);
         }
         return ret;
     }
