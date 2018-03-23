f20db0117c9f750ff70272673913f0a2df19058b
zhenyiluo <zhenyiluo@gmail.com>
2018-03-22 21:35:20 -0400

    Coin change

--- src/CoinChange/Solution.java
+++ src/CoinChange/Solution.java
@@ -1,31 +1,16 @@
-public class Solution {
+class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(coins == null || coins.length == 0 || amount < 0){
-            return -1;
-        }
-        if(amount == 0){
-            return 0;
-        }
-        int n = amount;
-        int[] dp = new int[n+1];
+        if(coins == null || coins.length == 0) return -1;
+        int[] dp = new int[amount+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-
-        for(int i = 0; i < n; i++){
-            if(dp[i] != Integer.MAX_VALUE){
-                for(int coin : coins){
-                    if(i + coin <= n){
-                        dp[i+coin] = Math.min(dp[i+coin], dp[i] + 1);
-                    }
+        for(int coin: coins){
+            for(int i = coin; i <= amount; i++){
+                if(dp[i-coin] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
                 }
             }
         }
-
-        if(dp[n] == Integer.MAX_VALUE){
-            return -1;
-        }else{
-            return dp[n];
-        }
-
+        return dp[amount] == Integer.MAX_VALUE ? -1: dp[amount];
     }
 }
