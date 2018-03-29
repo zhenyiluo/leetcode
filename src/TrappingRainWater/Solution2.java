edbe3908aea809abb4c5fe39832ef54dcb6d1333
zhenyiluo <zhenyiluo@gmail.com>
2018-03-29 09:25:59 -0400

    Trapping rain water solution 2

---
+++ src/TrappingRainWater/Solution2.java
@@ -0,0 +1,14 @@
+class Solution2 {
+    public int trap(int[] height) {
+        if(height == null || height.length <= 1) return 0;
+        int level = 0;
+        int i = 0;
+        int j = height.length -1;
+        int ret = 0;
+        while(i < j){
+            level = Math.min(height[i], height[j]) > level ? Math.min(height[i], height[j]) : level;
+            ret += height[i] < height[j] ? level - height[i++] : level - height[j--];
+        }
+        return ret;
+    }
+}
