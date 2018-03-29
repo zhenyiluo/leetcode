0f60611f891678432e58246f4b5cf1e87a2912f8
zhenyiluo <zhenyiluo@gmail.com>
2018-03-28 23:22:20 -0400

    Minimum window substring

--- src/MinimumWindowSubstring/Solution.java
+++ src/MinimumWindowSubstring/Solution.java
@@ -1,40 +1,42 @@
-public class Solution {
+class Solution {
     public String minWindow(String s, String t) {
-        int[] needToFind = new int[256];
+        if(s == null || t == null || s.length() < t.length()) return "";
         int[] hasFound = new int[256];
-        String ans = "";
-        int minWin = Integer.MAX_VALUE;
-        for(int i = 0; i < t.length(); i++) {
+        int[] needToFind = new int[256];
+
+        String ret = "";
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < t.length(); i++){
             needToFind[t.charAt(i)] ++;
         }
-        
         int count = 0;
-    
-        for(int begin = 0, end = 0; end < s.length(); end ++) {
-            char c = s.charAt(end);
-            if(needToFind[c] == 0) {
+        for(int begin = 0, end = 0; end < s.length(); end++){
+            char c1 = s.charAt(end);
+            if(needToFind[c1] == 0){
                 continue;
             }
-            hasFound[c]++;
-            if(hasFound[c] <= needToFind[c]) {
+
+            hasFound[c1]++;
+
+            if(hasFound[c1] <= needToFind[c1]){
                 count ++;
             }
-            
-            if(count == t.length()) {
-                while(needToFind[s.charAt(begin)] == 0 || needToFind[s.charAt(begin)] < hasFound[s.charAt(begin)]){
-                    if(needToFind[s.charAt(begin)] < hasFound[s.charAt(begin)]) {
-                        hasFound[s.charAt(begin)] --;
+
+            if(count == t.length()){
+                while(true){
+                    char c2 = s.charAt(begin);
+                    if(needToFind[c2] > 0 && needToFind[c2] >= hasFound[c2]){
+                        break;
                     }
+                    hasFound[c2]--;
                     begin ++;
-                } 
-                if(end - begin + 1 < minWin) {
-                    minWin = end - begin + 1;
-                    ans = s.substring(begin, end + 1);
+                }
+                if(end - begin + 1 < min){
+                    min = end - begin + 1;
+                    ret = s.substring(begin, end + 1);
                 }
             }
         }
-        return ans;
+        return ret;
     }
 }
-
-
