ea34b8619b8a71252dc54cfd50b7d5156db15942
zhenyiluo <zhenyiluo@gmail.com>
2018-04-19 08:02:29 -0400

    Shortest Palindrome

--- src/ShortestPalindrome/Solution.java
+++ src/ShortestPalindrome/Solution.java
@@ -1,19 +1,17 @@
-public class Solution {
-
+class Solution {
     public String shortestPalindrome(String s) {
-        int j = 0; 
-        for(int i = s.length()-1; i >= 0; i--){
+        int j = 0;
+        for(int i = s.length() -1 ; i >= 0; i--){
             if(s.charAt(i) == s.charAt(j)){
-                j++;
+                j ++;
             }
         }
         if(j == s.length()){
             return s;
         }
         String suffix = s.substring(j);
-        String prefix = new StringBuilder(suffix).reverse().toString();
+        String prefix = new StringBuilder(s.substring(j)).reverse().toString();
         String mid = shortestPalindrome(s.substring(0, j));
         return prefix + mid + suffix;
     }
-
 }
