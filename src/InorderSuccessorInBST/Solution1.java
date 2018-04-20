6e1130ab44b416be9e1ebc7019cf4c770ad4885b
zhenyiluo <zhenyiluo@gmail.com>
2018-04-20 08:12:31 -0400

    Change solution

--- src/InorderSuccessorInBST/Solution1.java
+++ src/InorderSuccessorInBST/Solution1.java
@@ -7,23 +7,13 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution1 {
+class Solution1 {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        if(root == null || p == null){
-            return null;
-        }
-        
+        if(root == null) return null;
         if(root.val <= p.val){
             return inorderSuccessor(root.right, p);
-        }else{
-            TreeNode left = inorderSuccessor(root.left, p);
-            if(left != null){
-                return left;
-            }else{
-                return root;
-            }
         }
+        TreeNode left = inorderSuccessor(root.left, p);
+        return left == null ? root : left;
     }
-    
-    
 }
