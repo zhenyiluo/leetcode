afab9386cfdd840fd8b891afbe3de03251495ca2
zhenyiluo <zhenyiluo@gmail.com>
2018-04-25 09:25:35 -0400

    Find Kth smallest pair distance

--- src/FindKthSmallestPairDistance/Solution2.java
+++ src/FindKthSmallestPairDistance/Solution2.java
@@ -1,22 +1,23 @@
 class Solution2 {
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
-        int n = nums.length, low = 0, high = nums[n-1] - nums[0];
-        while (low < high) {
-            int cnt = 0;
+        int low = 0;
+        int n = nums.length;
+        int high = nums[n -1] - nums[0];
+        while(low < high){
             int mid = low + (high - low) / 2;
-            for (int i = 0; i < n-1; i++) {
-                int j = i+1;
-                while (j < n && nums[j] - nums[i] <= mid) j++;
-                cnt += j - i-1;
+            int cnt = 0;
+            for(int i = 0; i < n -1; i ++){
+                int j = i + 1;
+                while(j < n && nums[j] - nums[i] <= mid) j ++;
+                cnt += j - i -1;
             }
-            if (cnt < k){
+            if(cnt < k){
                 low = mid + 1;
             }else{
                 high = mid;
             }
         }
-
         return low;
     }
 }
