dd2f4e2d9e8d4abd25e0463d1834a1575504064f
zhenyiluo <zhenyiluo@gmail.com>
2018-05-01 08:25:07 -0400

    Sentence Screen fitting

---
+++ src/SentenceScreenFitting/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        String s = String.join(" ", sentence) + " ";
+        int start = 0;
+        int len = s.length();
+        for(int i = 0; i < rows; i++){
+            start += cols;
+            if(s.charAt(start % len) == ' '){
+                start ++;
+            }else{
+                while(start > 0 && s.charAt((start -1) % len) != ' '){
+                    start --;
+                }
+            }
+        }
+        return start / len;
+    }
+}
