805013efcc1d88a6a5e4e79ea27d119802931381
zhenyiluo <zhenyiluo@gmail.com>
2018-05-08 09:25:57 -0400

    Minimum size subarray sum

--- src/MinimumSizeSubarraySum/Solution.java
+++ src/MinimumSizeSubarraySum/Solution.java
@@ -1,26 +1,20 @@
-public class Solution {
+class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
-        
-        int ret = Integer.MAX_VALUE;
+        if(nums == null || nums.length == 0) return 0;
         int start = -1;
+        int ret = Integer.MAX_VALUE;
         int cur = 0;
         int sum = 0;
         while(cur < nums.length){
             sum += nums[cur];
             if(sum >= s){
-                while(start+ 1 < nums.length && sum - nums[start+1] >= s){
+                while(start +1 < nums.length && sum - nums[start+1] >= s ){
                     sum -= nums[++start];
                 }
                 ret = Math.min(ret, cur - start);
             }
             cur++;
         }
-        if(ret == Integer.MAX_VALUE){
-            return 0;
-        }
-        return ret;
+        return ret == Integer.MAX_VALUE ? 0 : ret;
     }
 }
