36ccda4ec6ac1f4bf174e2bde395c15936b74442
zhenyiluo <zhenyiluo@gmail.com>
2018-05-09 08:51:04 -0400

    add two numbers

--- src/AddTwoNumbers/Solution.java
+++ src/AddTwoNumbers/Solution.java
@@ -8,7 +8,7 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ret = new ListNode(0);
+        ListNode ret = new ListNode(-1);
         ListNode cur = ret;
         int carryOver = 0;
         while(l1 != null || l2 != null){
@@ -21,9 +21,8 @@ class Solution {
                 sum += l2.val;
                 l2 = l2.next;
             }
-            int val = sum % 10;
             carryOver = sum / 10;
-            cur.next = new ListNode(val);
+            cur.next = new ListNode(sum % 10);
             cur = cur.next;
         }
         if(carryOver != 0){
