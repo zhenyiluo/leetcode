3568494da96a5eea1e3edd30c09bc6d17d3c41f4
zhenyiluo <zhenyiluo@gmail.com>
2018-05-11 09:05:10 -0400

    Number of islands

---
+++ src/NumberOfIslands/Solution3.java
@@ -0,0 +1,30 @@
+class Solution3 {
+    int[] dx = new int[]{-1, 1, 0, 0};
+    int[] dy = new int[]{0, 0, -1, 1};
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int ret = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    ret ++;
+                }
+            }
+        }
+        return ret;
+    }
+
+    private void dfs(char[][] grid, int x, int y){
+        grid[x][y] = '0';
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length && grid[nx][ny] == '1'){
+                dfs(grid, nx, ny);
+            }
+        }
+    }
+}
