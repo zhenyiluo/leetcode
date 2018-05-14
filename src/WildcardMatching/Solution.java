04f4bc095278a3a25cd9cc8ba99dd6e62df23660
zhenyiluo <zhenyiluo@gmail.com>
2018-05-14 09:58:48 -0400

    Wildcard Matching

--- src/WildcardMatching/Solution.java
+++ src/WildcardMatching/Solution.java
@@ -5,10 +5,6 @@ class Solution {
         int plen = p.length();
         boolean[][] dp = new boolean[slen+1][plen+1];
         dp[0][0] = true;
-        for(int i = 1; i <= slen; i++){
-            dp[i][0] = false;
-        }
-
         for(int i = 1; i <= plen; i++){
             if(p.charAt(i-1) == '*'){
                 dp[0][i] = dp[0][i-1];
@@ -17,10 +13,10 @@ class Solution {
 
         for(int i = 1; i <= slen; i++){
             for(int j = 1; j <= plen; j++){
-                if((s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') && dp[i-1][j-1]){
-                    dp[i][j] = true;
-                }else if(p.charAt(j-1) == '*' && (dp[i-1][j] || dp[i][j-1])){
-                    dp[i][j] = true;
+                if((s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') ){
+                    dp[i][j] = dp[i-1][j-1];
+                }else if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
                 }
             }
         }
