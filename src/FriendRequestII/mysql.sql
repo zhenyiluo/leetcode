a5745c315829e0e447075b7369f9d40033167d74
zhenyiluo <zhenyiluo@gmail.com>
2018-06-15 09:36:15 -0700

    Friend Request II

---
+++ src/FriendRequestII/mysql.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select id1 as id, count(id2) as num from
+(select requester_id as id1, accepter_id as id2 from request_accepted
+union all
+ select accepter_id as id1, requester_id as id2 from request_accepted
+) as tmp
+group by id1
+order by num desc limit 1
