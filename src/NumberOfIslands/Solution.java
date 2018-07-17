6b590c6dfcd0de5f9252cee605c5b6605370977b
zhenyiluo <zhenyiluo@gmail.com>
2018-07-17 08:30:16 -0700

    Number of islands

--- src/NumberOfIslands/Solution.java
+++ src/NumberOfIslands/Solution.java
@@ -1,41 +1,39 @@
-public class Solution {
-    static final int[] dx = {-1, 1, 0, 0};
-    static final int[] dy = {0, 0, -1, 1};
+class Solution {
+    private static final int[] dx = new int[]{-1, 1, 0, 0};
+    private static final int[] dy = new int[]{0, 0, -1, 1};
     public int numIslands(char[][] grid) {
-        int ret = 0;
         if(grid == null || grid.length == 0 || grid[0].length == 0){
-            return ret;
+            return 0;
         }
         int m = grid.length;
         int n = grid[0].length;
-        for(int i = 0; i < m; i++){
+        int ret = 0;
+        for(int i = 0; i < m ; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
                     ret ++;
-                    check(i, j, grid);
+                    bfs(grid, i, j);
                 }
             }
         }
         return ret;
     }
-    
-    private void check(int x, int y, char[][] grid){
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(x);
-        q.add(y);
+
+    private void bfs(char[][] grid, int x, int y){
         grid[x][y] = '0';
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
         while(!q.isEmpty()){
-            int tmpX = q.poll();
-            int tmpY = q.poll();
-            for(int i = 0; i < 4; i++){
-                int nx = tmpX + dx[i];
-                int ny = tmpY + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == '1'){
-                    grid[nx][ny] = '0';
-                    q.add(nx);
-                    q.add(ny);
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                int[] pair = q.poll();
+                for(int j = 0; j < 4; j++){
+                    int nx = pair[0] + dx[j];
+                    int ny = pair[1] + dy[j];
+                    if(nx >= 0 && ny >= 0 && nx < grid.length && ny < grid[0].length && grid[nx][ny] == '1'){
+                        grid[nx][ny] = '0';
+                        q.add(new int[]{nx, ny});
+                    }
                 }
             }
         }
--- src/NumberOfIslands/Solution3.java
+++ src/NumberOfIslands/Solution3.java
@@ -1,16 +1,18 @@
-class Solution3 {
-    int[] dx = new int[]{-1, 1, 0, 0};
-    int[] dy = new int[]{0, 0, -1, 1};
+class Solution {
+    private static final int[] dx = new int[]{-1, 1, 0, 0};
+    private static final int[] dy = new int[]{0, 0, -1, 1};
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
         int m = grid.length;
         int n = grid[0].length;
         int ret = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
                     ret ++;
+                    dfs(grid, i, j);
                 }
             }
         }
