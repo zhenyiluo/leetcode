30598a571dee3cec39f3eb8d4a79ac0b535037a2
zhenyiluo <zhenyiluo@gmail.com>
2018-07-22 18:38:33 -0700

    Range Sum Query

---
+++ src/RangeSumQuery/Solution.java
@@ -0,0 +1,41 @@
+class NumArray {
+    int len;
+    int[] tree;
+    int[] vals;
+    public NumArray(int[] nums) {
+        len = nums.length;
+        tree = new int[len+1];
+        vals = new int[len];
+        for(int i = 0; i < len; i++){
+            update(i, nums[i]);
+        }
+    }
+
+    public void update(int i, int val) {
+        int diff = val - vals[i];
+        if(diff == 0) return;
+        vals[i] = val;
+        for(int j = i + 1; j <= len; j += j & (-j)){
+            tree[j] += diff;
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return query(j + 1) - query(i);
+    }
+
+    private int query(int i){
+        int ret = 0;
+        for(int j = i; j > 0; j -= j & (-j)){
+            ret += tree[j];
+        }
+        return ret;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
