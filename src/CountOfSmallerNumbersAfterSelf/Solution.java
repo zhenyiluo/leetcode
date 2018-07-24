3af9b40dc0fd8d303663799a67a4e5e2ed9f0f9d
zhenyiluo <zhenyiluo@gmail.com>
2018-07-24 09:29:10 -0700

    another solution

--- src/CountOfSmallerNumbersAfterSelf/Solution.java
+++ src/CountOfSmallerNumbersAfterSelf/Solution.java
@@ -1,30 +1,29 @@
-public class Solution {
+class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        List<Integer> ret = new LinkedList<>();
+        List<Integer> ret = new ArrayList<>();
         if(nums == null || nums.length == 0){
             return ret;
         }
-        int len = nums.length;
-        ArrayList<Integer> sorted = new ArrayList<>();
-        for(int i = len -1; i>= 0; i--){
-            int index = helper(sorted, nums[i]);
+        ArrayList<Integer> sortList = new ArrayList<>();
+        for(int i = nums.length -1; i >= 0; i--){
+            int index = helper(sortList, nums[i]);
             ret.add(0, index);
-            sorted.add(index, nums[i]);
+            sortList.add(index, nums[i]);
         }
         return ret;
     }
 
-    private int helper(ArrayList<Integer> sorted, int num){
-        int low = 0;
-        int high = sorted.size() - 1;
-        while(low <= high){
-            int mid = low + ((high - low) >> 1);
-            if(sorted.get(mid) < num){
-                low = mid + 1;
+    private int helper(ArrayList<Integer> sortList, int target){
+        int left = 0;
+        int right = sortList.size() - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(sortList.get(mid) < target){
+                left = mid + 1;
             }else{
-                high = mid - 1;
+                right = mid - 1;
             }
         }
-        return low;
+        return left;
     }
 }
