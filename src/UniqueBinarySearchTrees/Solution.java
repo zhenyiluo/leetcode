da500a5457441418fdf3fdd205849ad602663f1b
zhenyiluo <zhenyiluo@gmail.com>
2018-07-30 09:25:13 -0700

    Unique Binary Search Trees

--- src/UniqueBinarySearchTrees/Solution.java
+++ src/UniqueBinarySearchTrees/Solution.java
@@ -1,22 +1,13 @@
-public class Solution {
+class Solution {
     private HashMap<Integer, Integer> cache = new HashMap<>();
     public int numTrees(int n) {
-        if(n < 0) {
-            return 0;
+        if(n <= 1) return 1;
+        if(cache.containsKey(n)) return cache.get(n);
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += numTrees(i-1) * numTrees(n - i);
         }
-        
-        if(n <=1) {
-            return 1;
-        }
-        
-        if(!cache.containsKey(n)) {
-            int ans = 0;
-            for(int i = 1; i <= n; i++) {
-                ans += numTrees(i-1) * numTrees(n - i);
-            }
-            cache.put(n, ans);
-        }
-        
-        return cache.get(n);
+        cache.put(n, sum);
+        return sum;
     }
 }
