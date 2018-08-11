da1823ca1c978e255d1b15bab5c23c433b753dc0
zhenyiluo <zhenyiluo@gmail.com>
2018-08-10 18:28:49 -0700

    4Sum

--- src/4Sum/Solution.java
+++ src/4Sum/Solution.java
@@ -1,41 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ret = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         HashSet<List<Integer>> hs = new HashSet<>();
-        if(nums == null || nums.length < 4) {
-            return ret;
-        }
-
-        Arrays.sort(nums);
+        if(nums == null || nums.length < 4) return ans;
         int len = nums.length;
-        int k, p, sum;
+        Arrays.sort(nums);
         for(int i = 0; i < len -3; i++){
-            for(int j = i+1; j < len -2; j++) {
-                k = j+1;
-                p = len -1;
-                while(k < p){
-                    sum = nums[i] + nums[j] + nums[k] + nums[p];
-                    if(sum == target) {
-                        List<Integer> tmp = new ArrayList<>();
-                        tmp.add(nums[i]);
-                        tmp.add(nums[j]);
-                        tmp.add(nums[k]);
-                        tmp.add(nums[p]);
-                        if(!hs.contains(tmp)){
-                            hs.add(tmp);
-                            ret.add(tmp);
+            for(int j = i+1; j < len -2; j++){
+                int k = j + 1;
+                int l = len -1;
+                while(k < l){
+                    long sum = nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target){
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        list.add(nums[l]);
+                        if(!hs.contains(list)){
+                            hs.add(list);
+                            ans.add(list);
                         }
                         k++;
-                        p--;
-                    }else if(sum < target) {
+                        l--;
+                    }else if(sum < target){
                         k++;
                     }else{
-                        p--;
+                        l--;
                     }
                 }
-
             }
         }
-        return ret;
+        return ans;
     }
 }
