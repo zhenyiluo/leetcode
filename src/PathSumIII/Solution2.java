eb5ceedbd885929dc4dcebdf262496a3dac8f653
zhenyiluo <zhenyiluo@gmail.com>
2018-08-12 17:59:44 -0700

    Path sum III

---
+++ src/PathSumIII/Solution2.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+        return dfs(root, 0, sum, hm);
+    }
+
+    private int dfs(TreeNode node, int sum, int target, HashMap<Integer, Integer> hm){
+        if(node == null) return 0;
+        sum += node.val;
+        int res = hm.getOrDefault(sum - target, 0);
+        hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+        res += dfs(node.left, sum, target, hm) + dfs(node.right, sum, target, hm);
+        hm.put(sum, hm.get(sum) -1);
+        return res;
+    }
+}
