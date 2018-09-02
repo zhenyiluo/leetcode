959ee3646d8d5b938e8e7f9048dee0b1703509e0
zhenyiluo <zhenyiluo@gmail.com>
2018-09-01 19:52:50 -0700

    Max sum of rectangle no larger than K

---
+++ src/MaxSumOfRectangleNoLargerThanK/Solution3.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return 0;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int max = Integer.MIN_VALUE;
+        if(row < col){
+            for(int i = 0; i< row; i++){
+                int[] colSum = new int[col];
+                for(int j = i; j< row; j++){
+                    for(int c = 0; c< col; c++){
+                        colSum[c] += matrix[j][c];
+                    }
+                    max = Math.max(max, maxSumSubarray(colSum, k));
+                }
+            }
+        }else{
+            for(int i = 0; i< col; i++){
+                int[] rowSum = new int[row];
+                for(int j = i; j< col; j++){
+                    for(int r = 0; r< row; r++){
+                        rowSum[r] += matrix[r][j];
+                    }
+                    max = Math.max(max, maxSumSubarray(rowSum, k));
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private int maxSumSubarray(int[] nums, int k){
+        int sum = 0;
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(0);
+        int max = Integer.MIN_VALUE;
+        for(int num: nums){
+            sum += num;
+            Integer prev = ts.ceiling(sum - k);
+            if(prev != null) max = Math.max(max, sum - prev);
+            ts.add(sum);
+        }
+        return max;
+    }
+}
