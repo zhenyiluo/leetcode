126e1b14e78471828efc49cec6827551f05516fc
zhenyiluo <zhenyiluo@gmail.com>
2018-09-02 18:08:46 -0700

    Wildcard matching

---
+++ src/WildcardMatching/Solution3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int slen = s.length();
+        int plen = p.length();
+        boolean[][] dp = new boolean[slen+1][plen+1];
+        dp[0][0] = true;
+        for(int i = 1; i <= plen; i++){
+            if(p.charAt(i-1) == '*' && dp[0][i-1]){
+                dp[0][i] = true;
+            }else{
+                break;
+            }
+        }
+
+        for(int i = 0; i < slen; i++){
+            for(int j = 0; j < plen; j++){
+                if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+                    dp[i+1][j+1] = dp[i][j];
+                }else if(p.charAt(j) == '*'){
+                    dp[i+1][j+1] = dp[i][j] || dp[i+1][j] || dp[i][j+1];
+                }
+            }
+        }
+        return dp[slen][plen];
+    }
+}
