91a68f35c3dc35011d5fe8787d8612f10ab4db95
zhenyiluo <zhenyiluo@gmail.com>
2018-09-23 17:27:03 -0700

    Coin Path

---
+++ src/CoinPath/Solution3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> cheapestJump(int[] A, int B) {
+        List<Integer> ans = new ArrayList<>();
+        if(A == null || A.length == 0) return ans;
+        int n = A.length;
+        int[] dp = new int[n];
+        int[] forward = new int[n];
+        for(int i = 0; i < n; i++){
+            dp[i] = Integer.MAX_VALUE;
+            forward[i] = -1;
+        }
+        dp[n-1] = A[n-1];
+        for(int i = n-2; i >= 0; i--){
+            if(A[i] == -1) continue;
+            for(int j = i + 1; j <= Math.min(i + B, n-1); j++){
+                if(dp[j] == Integer.MAX_VALUE || A[j] == -1) continue;
+                if(dp[i] > A[i] + dp[j]){
+                    dp[i] = A[i] + dp[j];
+                    forward[i] = j;
+                }
+            }
+        }
+        if(dp[0] == Integer.MAX_VALUE) return ans;
+        int k = 0;
+        while(k != -1){
+            ans.add(k + 1);
+            k = forward[k];
+        }
+        return ans;
+    }
+}
