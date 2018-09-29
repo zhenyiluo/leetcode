91b8b6ed9ffa2dce86506911455c4e0222c72a1e
zhenyiluo <zhenyiluo@gmail.com>
2018-09-29 11:53:45 -0700

    add few solutions

---
+++ src/IsSubsequence/Solution8.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s == null || t == null) return false;
+        if(s.length() == 0) return true;
+        if(t.length() == 0) return false;
+        int indexS = 0;
+        int indexT = 0;
+        while(indexS < s.length() && indexT < t.length()){
+            if(s.charAt(indexS) == t.charAt(indexT)){
+                indexS ++;
+                indexT ++;
+            }else{
+                indexT ++;
+            }
+            if(indexS == s.length()) return true;
+        }
+        return false;
+    }
+}
