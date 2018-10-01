2d5ea8f18e7178b2629997d8584a9bf8c395e768
zhenyiluo <zhenyiluo@gmail.com>
2018-10-01 09:25:59 -0700

    Reverse LinkedList

---
+++ src/ReverseLinkedList/Solution4.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode cur = head;
+        while(cur != null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
