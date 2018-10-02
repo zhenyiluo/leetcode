b5322241c69d31e4754d74f59674a5b6cfee9da0
zhenyiluo <zhenyiluo@gmail.com>
2018-10-02 09:28:03 -0700

    remove duplicate

---
+++ src/RemoveDuplicatesFromSortedList/Solution2.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = head;
+        while(cur != null){
+            while(cur.next != null && cur.val == cur.next.val){
+                cur = cur.next;
+            }
+            prev.next = cur;
+            prev = prev.next;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
