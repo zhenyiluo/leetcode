122e2c25f727244bd83a4e9b73a0b398edda404a
zhenyiluo <zhenyiluo@gmail.com>
2018-10-06 19:00:04 -0700

    Longest substring with at most two distinct characters

---
+++ src/LongestSubstringWithAtMostTwoDistinctCharacters/Solution2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int i = 0;
+        int ans = 0;
+        for(int j = 0; j < s.length(); j++){
+            char r = s.charAt(j);
+            hm.put(r, hm.getOrDefault(r, 0) + 1);
+            while(hm.size() > 2){
+                char l = s.charAt(i++);
+                hm.put(l, hm.get(l) - 1);
+                if(hm.get(l) == 0){
+                    hm.remove(l);
+                }
+            }
+            ans = Math.max(ans, j - i + 1);
+        }
+        return ans;
+    }
+}
