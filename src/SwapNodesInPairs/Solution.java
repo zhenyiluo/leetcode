72c1c75b0f0b3de5a1b497afbdc1ece05a534f2b
zhenyiluo <zhenyiluo@gmail.com>
2018-10-07 20:31:32 -0700

    Swap nodes in pairs

--- src/SwapNodesInPairs/Solution.java
+++ src/SwapNodesInPairs/Solution.java
@@ -10,7 +10,7 @@ class Solution {
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
         ListNode newHead = head.next;
-        head.next = swapPairs(newHead.next);
+        head.next = swapPairs(head.next.next);
         newHead.next = head;
         return newHead;
     }
