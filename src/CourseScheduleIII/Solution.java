dbe5398eec527e32e88f2d55645b843d795bba16
zhenyiluo <zhenyiluo@gmail.com>
2018-10-11 20:25:40 -0700

    Course Schedule III

---
+++ src/CourseScheduleIII/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> (a[1] - b[1]));
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> (b - a));
+        int time = 0;
+        for(int[] course: courses){
+            time += course[0];
+            pq.add(course[0]);
+            if(time > course[1]){
+                time -= pq.poll();
+            }
+        }
+        return pq.size();
+    }
+}
